baf0657d50cc0770ecc6a184181b1c72a45f5978
nicholasdegrate <nicholasdegrate@gmail.com>
2021-02-01 11:43:28 -0600

    done

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+
+end
+
+# under dog class define three dogs 
+# NO Dog.new() because a initialize wasnt defined
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+
+end
+
+# NO Person.new() because a initialize wasnt defined
+adele_goldberg = Person.new
+alan_kay = Person.new
